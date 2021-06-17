6d223cc5e150c776e04a01e9e458647d1444b57c
Mesdour Sofiane <mesdour_sofiane@hotmail.fr>
2021-06-17 23:28:00 +0200

    declartion de la fonction getBookDetails pour la fair call sur les autres partie du code

--- P2_01_GetBooks.py
+++ P2_01_GetBooks.py
@@ -4,52 +4,66 @@ import pandas as pd
 
 from bs4 import BeautifulSoup
 
-dataBook =[]
-response = requests.get(url)
-response.encoding='UTF-8'
+#url='https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html'
 
-if response.ok:
-   html =BeautifulSoup(response.text ,'lxml',)
-   tabs= html.find('table')
-   tabs=BeautifulSoup(str(tabs),'lxml')
-   tds=tabs.findAll('td')
+def getBookDetails(url):
+   dataBook =[]
+   response = requests.get(url)
+   response.encoding='UTF-8'
+   if response.ok:
+       html =BeautifulSoup(response.text ,'lxml',)
 
-   title=html.find('h1')
-       
-   ps=html.findAll('p')
+       tabs= html.find('table')
+       tabs=BeautifulSoup(str(tabs),'lxml')
+       tds=tabs.findAll('td')
 
-   imgs=html.find('img')
-   imgString=str(imgs['src'])
-   imgString=imgString[5:]
-   imgString='https://books.toscrape.com'+imgString
+       title=html.find('h1')
+       
+       ps=html.findAll('p')
 
-   numAvailableStr=str(tds[5])
-   num=([int(s) for s in re.findall(r'-?\d+\.?\d*', numAvailableStr)])
-   numAvailable=num[0]
+       imgs=html.find('img')
+       imgString=str(imgs['src'])
+       imgString=imgString[5:]
+       imgString='https://books.toscrape.com'+imgString
        
-   prod=str(ps[3].text)
+       numAvailableStr=str(tds[5])
+       num=([int(s) for s in re.findall(r'-?\d+\.?\d*', numAvailableStr)])
+       numAvailable=num[0]
+       
+       prod=str(ps[3].text)
 
-   upc=tds[0].text
+
        
-   cat=tds[1].text
+       upc=tds[0].text
        
-   prixH=tds[2].text
+       cat=tds[1].text
        
-   prixT=tds[3].text
+       prixH=tds[2].text
        
-   rating=str(ps[2]['class'][1])
+       prixT=tds[3].text
+       
+       rating=str(ps[2]['class'][1])
        
        
       
-   dataBook.append (url)
-   dataBook.append (upc)
-   dataBook.append (title.text)
-   dataBook.append (prixH)
-   dataBook.append (prixT)
-   dataBook.append (numAvailable)
-   dataBook.append (prod)
-   dataBook.append (cat)
-   dataBook.append (rating)
-   dataBook.append(imgString)
+       dataBook.append (url)
+       dataBook.append (upc)
+       dataBook.append (title.text)
+       dataBook.append (prixH)
+       dataBook.append (prixT)
+       dataBook.append (numAvailable)
+       dataBook.append (prod)
+       dataBook.append (cat)
+       dataBook.append (rating)
+       dataBook.append(imgString)
 
-   print(dataBook)
+       print(dataBook)
+       return dataBook
+   
+
+#fin de Fonction
+
+
+
+
+# test de la fonction print(print(getBookDetails('https://books.toscrape.com/catalogue/the-mysterious-affair-at-styles-hercule-poirot-1_452/index.html'))
