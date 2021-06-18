5fd9c811552ad46fa13d4c23f248c6266a5386ce
Mesdour Sofiane <mesdour_sofiane@hotmail.fr>
2021-06-18 02:20:47 +0200

    Update P2_02_getBooksInPage.py
    creation de la fonction getPage

--- P2_02_getBooksInPage.py
+++ P2_02_getBooksInPage.py
@@ -5,7 +5,10 @@ from getBook import getBookDetails
 
 #url =('https://books.toscrape.com/catalogue/category/books/mystery_3/page-2.html')
 
+def getPage(url,root):
 	pageBooks=[]
+
+
 
 	response = requests.get(url)
 
@@ -28,3 +31,11 @@ from getBook import getBookDetails
 	    		ahref=ahref[5:]
 	    	
 	    	ahref='https://books.toscrape.com/catalogue'+ahref
+	    	booksDetails=getBookDetails(ahref)
+
+	    	pageBooks.append (booksDetails)
+
+	return (pageBooks)
+
+# dans le cas d'apple de la page d'une rubrique print(getPage('https://books.toscrape.com/catalogue/category/books/mystery_3/page-2.html',False))
+# dans le cas d'apple de la page de All books 	print(getPage('https://books.toscrape.com/catalogue/page-1.html'),True)
