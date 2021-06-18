67b26d9de8704958e5d59438c686824885c1063d
Mesdour Sofiane <sofianeazzouz@gmail.com>
2021-06-18 02:38:47 +0200

    Create P02_05_getUrl.py
    importation  des fonction

---
+++ P02_05_getUrl.py
@@ -0,0 +1,10 @@
+import autopep8
+import requests
+import re
+import csv
+import pandas as pd
+import os
+
+from bs4 import BeautifulSoup
+from getCategory import getCategoryBooks
+from getImages import saveImages
